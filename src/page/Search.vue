890b5332c3262b11f0a0fc319f2fd39e99b7fccc
DESKTOP-0VOMT7I\xw <985671526@qq.com>
2020-07-03 17:23:54 +0800

    直播

---
+++ src/page/Search.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="Search">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Search",
+        data (){
+            return {}
+        },
+        methods: {
+
+        },
+        created() {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
